31d15ad37f1b5646ec54fe75b23bcfea144bee92
Tim Kalan <timkalan99@gmail.com>
2020-02-15 16:03:45 +0100

    Začel delo z zemljevidi

--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -1,14 +1,26 @@
-zemljevidStock <- tm_shape(merge(svet, stock %>% 
-                                   filter(age == "total", year == "2019") %>%
-                                   select(-"year", -"age") %>% 
-                                   group_by(country) %>% 
-                                   summarise(number = sum(number, na.rm = TRUE)) %>%
-                                   inner_join(drzave[c("country", "leto", "populacija")] %>% 
-                                                filter(leto == "2018")) %>%
-                                   mutate(delez = number / populacija) %>%
-                                   select(-"leto", -"number", -"populacija"), 
+# ZEMLJEVIDI ####
+delezi <- stock %>%  
+  filter(age == "total", year == "2019") %>%
+  select(-"year", -"age") %>% 
+  group_by(country) %>% 
+  summarise(number = sum(number, na.rm = TRUE)) %>%
+  inner_join(drzave[c("country", "leto", "populacija")] %>% 
+               filter(leto == "2018")) %>%
+  mutate(delez = number / populacija) %>%
+  select(-"leto", -"number", -"populacija")
+
+zemStock <- tm_shape(merge(svet, delezi, 
                                  by.x = "NAME", by.y = "country")) + 
-  tm_polygons("delez")
+  tm_polygons("delez", midpoint = 0.4)
+
+skupineDelez <- kmeans(delezi$delez, 3, nstart = 1500)
+zemCluster <- tm_shape(merge(svet, data.frame(country = delezi$country, skupina = factor(skupineDelez$cluster)), 
+                    by.x = "NAME", by.y = "country")) + 
+  tm_polygons("skupina")
+
+
+# GRAFI ####
+
 
 
 # # Izračun neto migracije za posamezne države skozi čas
