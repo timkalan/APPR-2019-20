7125dadf7e3bbad8181ce7f17ed086c6c0859d47
Tim Kalan <timkalan99@gmail.com>
2020-02-15 16:04:13 +0100

    Razvrščanje - deleži priseljencev

--- analiza/analiza.r
+++ analiza/analiza.r
@@ -1,11 +1,18 @@
-# 4. faza: Analiza podatkov
+skupineDelez <- kmeans(delezi$delez, 3, nstart = 1500)
+zemCluster <- tm_shape(merge(svet, data.frame(country = delezi$country, skupina = factor(skupineDelez$cluster)), 
+                             by.x = "NAME", by.y = "country")) + 
+  tm_polygons("skupina")
 
-podatki <- obcine %>% transmute(obcina, povrsina, gostota,
-                                gostota.naselij=naselja/povrsina) %>%
-  left_join(povprecja, by="obcina")
-row.names(podatki) <- podatki$obcina
-podatki$obcina <- NULL
 
-# Število skupin
-n <- 5
-skupine <- hclust(dist(scale(podatki))) %>% cutree(n)
+
+# # 4. faza: Analiza podatkov
+# 
+# podatki <- obcine %>% transmute(obcina, povrsina, gostota,
+#                                 gostota.naselij=naselja/povrsina) %>%
+#   left_join(povprecja, by="obcina")
+# row.names(podatki) <- podatki$obcina
+# podatki$obcina <- NULL
+# 
+# # Število skupin
+# n <- 5
+# skupine <- hclust(dist(scale(podatki))) %>% cutree(n)
