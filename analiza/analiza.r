bcecfa2a1e1e45298e30e7564fcf2293df57312c
Tim Kalan <timkalan99@gmail.com>
2020-02-29 16:49:21 +0100

    razvrščanje v države

--- analiza/analiza.r
+++ analiza/analiza.r
@@ -1,5 +1,8 @@
 # ZEMLJEVID - CLUSTERING ####
-skupineDelez <- kmeans(delezi$delez, 5, nstart = 1500)
+skupineDelez <- kmeans(delezi$delez, 5, nstart = 1500) 
+centers <- sort(skupineDelez$centers) # uredimo skupine po deležu
+skupineDelez <- kmeans(delezi$delez, centers = centers, nstart = 1500)
+
 zemljevidCluster <- tm_shape(merge(svet, data.frame(country = delezi$country, 
                                                     skupina = factor(skupineDelez$cluster)), 
                              by.x = "NAME", by.y = "country")) + 
@@ -7,9 +10,8 @@ zemljevidCluster <- tm_shape(merge(svet, data.frame(country = delezi$country,
   tm_polygons("skupina", title = "Skupina") +
   tm_layout(main.title = "Države razdeljene v skupine glede na delež priseljencev") + 
   tm_legend(position = c("left", "center"))
-zemljevidCluster
 
-napakaClust <- skupineDelez$tot.withinss
+napakaClust1 <- skupineDelez$tot.withinss
 
 
 
@@ -23,13 +25,19 @@ korelacija <- cor(master$BDP, y = master$imigracija, use = "na.or.complete")
 grafMigracija <- ggplot(master %>% gather(preselitev, stevilo, 8:9) %>%
                                             group_by(leto, preselitev) %>% 
                                             summarise(stevilo = mean(stevilo)),
-                         aes(x = leto, y = stevilo)) + geom_point() +
+                         aes(x = leto, y = stevilo)) + 
   facet_grid(~preselitev) + xlim(1990, 2025) + 
   geom_smooth(method = "lm", se = FALSE, fullrange = TRUE) + 
   xlab("Leto") + ylab("Število") + 
   ggtitle("Povprečna emigracija in imigracija v obdobju 1990-2019") + 
-  geom_smooth(method = "loess", se = FALSE, fullrange = TRUE, col = "yellow")
+  geom_smooth(method = "loess", se = FALSE, fullrange = TRUE, col = "yellow") + 
+  geom_point()
+grafMigracija
 
+grafTest <- ggplot(master %>% gather(preselitev, stevilo, 8:9), 
+                   aes(x = leto, y = stevilo, group = leto)) + 
+  facet_grid(~preselitev) +
+  geom_boxplot() + scale_y_log10()
 
 cor(imigracija$Year, y = imigracija$number, use = "na.or.complete")
 
@@ -39,7 +47,6 @@ napoved$imigracija <- predict(linImigracija, napoved) # kaj model napove
 
 linEmigracija <- lm(data = master, emigracija ~ leto)
 napoved$emigracija <- predict(linEmigracija, napoved)
-# loh bi še kej o vsaki državi posebi. 
 
 loessImigracija <- loess(data = master, imigracija ~ leto)
 napoved$lImi <- predict(loessImigracija, napoved)
@@ -47,9 +54,40 @@ napoved$lImi <- predict(loessImigracija, napoved)
 loessEmigracija <- loess(data = master, emigracija ~ leto)
 napoved$lEmi <- predict(loessEmigracija, napoved)
 
-
 # napake <- sapply(list(linImigracija, kv, mls, mgam), function(x) sum(x$residuals^2))
 
 
+
 # RAZVRŠČANJE DRŽAV V SKUPINE ####
+povprecje <- master %>% group_by(country) %>% 
+  summarise(emigracija = mean(emigracija, na.rm = TRUE),
+            imigracija = mean(imigracija, na.rm = TRUE), 
+            populacija = mean(populacija, na.rm = TRUE)) %>%
+  mutate(emigracija = emigracija / populacija, imigracija = imigracija / populacija) %>%
+  select(-"populacija")
+
+skupineEmigracija <- kmeans(povprecje$emigracija, 6, nstart = 1500)
+centersEmi <- sort(skupineEmigracija$centers)
+skupineEmigracija <- kmeans(povprecje$emigracija, centers = centersEmi, nstart = 1500)
+
+zemljevidEmi <- tm_shape(merge(svet, data.frame(country = povprecje$country, 
+                                                    skupina = factor(skupineEmigracija$cluster)), 
+                                   by.x = "NAME", by.y = "country")) + 
+  tmap_options(max.categories = 6) + 
+  tm_polygons("skupina", title = "Skupina") + 
+  tm_layout(main.title = "Države razdeljene v skupine glede na povprečno emigracijo") + 
+  tm_legend(position = c("left", "center"))
+
+
+skupineImigracija <- kmeans(povprecje$imigracija, 6, nstart = 1500)
+centersImi <- sort(skupineImigracija$centers)
+skupineImigracija <- kmeans(povprecje$imigracija, centers = centersImi, nstart = 1500)
+
+zemljevidImi <- tm_shape(merge(svet, data.frame(country = povprecje$country, 
+                                                skupina = factor(skupineImigracija$cluster)), 
+                               by.x = "NAME", by.y = "country")) + 
+  tmap_options(max.categories = 6) + 
+  tm_polygons("skupina", title = "Skupina") + 
+  tm_layout(main.title = "Države razdeljene v skupine glede na povprečno imigracijo") + 
+  tm_legend(position = c("left", "center"))
 
